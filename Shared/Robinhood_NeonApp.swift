440d83ccb2985c614230e3bc708816b130fb8dff
Joshua Jiang <zhuoshengjiang@gmail.com>
2022-04-24 13:48:06 -0400

    Initial Commit

---
+++ Shared/Robinhood_NeonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Robinhood_NeonApp.swift
+//  Shared
+//
+//  Created by Joshua Jiang on 4/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Robinhood_NeonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
